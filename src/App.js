fb4273915722e7ea43c19f2cadb793ff5016670a
koltukustu-zbook <semih.babacan@std.yildiz.edu.tr>
2022-03-04 01:01:00 +0300

    imgDisplay function added, this function adds the functionality of showing one images after another

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 // import "./App.css";
 import { useState } from "react";
 import { create } from "ipfs-http-client";
-
 const client = create("https://ipfs.infura.io:5001/api/v0");
 
 function App() {
-  const [fileUrl, updateFileUrl] = useState(``);
+  // const [fileUrl, updateFileUrl] = useState(``);
+  const [images, updateImages] = useState("");
 
+  async function addImages(imgFileUrl) {
+    let imgElement = imgFileUrl && <img src={imgFileUrl} width="600px" />;
+    updateImages([...images, imgElement]);
+  }
   async function onChange(e) {
     const file = e.target.files[0];
     try {
@@ -16,16 +20,19 @@ function App() {
       const added = await client.add(file);
       const url = `https://ipfs.infura.io/ipfs/${added.path}`;
       console.log(url);
-      updateFileUrl(url);
+      // updateFileUrl(url);
+      addImages(url);
     } catch (error) {
       console.log("Error uploading file: ", error);
     }
   }
   return (
     <div className="App">
-      <h1>IPFS Uploading Example</h1>
-      <input type="file" onChange={onChange}/>
-      {fileUrl && <img src={fileUrl} width="600px" />}
+      <div className="Ipfs-holder">
+        <h1>IPFS Uploading Example</h1>
+        <input type="file" onChange={onChange} />
+      </div>
+      <div className="imgDisplay">{images}</div>
     </div>
   );
 }
